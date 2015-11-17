e1de407c66192e84151fcee2524abfe88a340e95
David Merlinsky <dmerlinsky@stuy.edu>
2015-11-17 09:44:41 -0500

    did some stuff

---
+++ Rational.java
@@ -0,0 +1,21 @@
+public class Rational{
+    private int numerator;
+    private int denominator;
+    public Rational(){
+	numerator = 0;
+	denominator =1;
+    }
+    public Rational(int num, int den){
+	if (den==0){
+	    System.out.print("Denominator cannot be zero");
+	    denominator=1;
+	    numerator=0;
+	    return;
+	}
+	numerator=num;
+	denominator=den;
+    }
+    public String toString(){
+	return numerator +"/"+ denominator;
+    }
+} 
