b939ba943a7b400465128c1c60e6331f36f464da
ThisIsMyNick <ThisIsMyNick@users.noreply.github.com>
2015-11-17 21:18:17 -0500

    add multiply, divide, reduction. change formatting and fix issues

--- Rational.java
+++ Rational.java
@@ -1,24 +1,54 @@
-public class Rational{
+public class Rational
+{
     private int numerator;
     private int denominator;
-    public Rational(){
-	numerator = 0;
-	denominator =1;
+    public Rational()
+    {
+        numerator = 0;
+        denominator = 1;
     }
-    public Rational(int num, int den){
-	if (den==0){
-	    System.out.print("Denominator cannot be zero");
-	    denominator=1;
-	    numerator=0;
-	    return;
-	}
-	numerator=num;
-	denominator=den;
+    public Rational(int num, int den)
+    {
+        if (den == 0)
+        {
+            System.out.print("Denominator cannot be zero");
+            numerator=0;
+            denominator=1;
+            return;
+        }
+        numerator=num;
+        denominator=den;
+        reduce();
     }
-    public String toString(){
-	return numerator +"/"+ denominator;
+    public String toString()
+    {
+        return numerator + "/" + denominator;
     }
-    public static double floatValue(){
-	return (double) numerator / denominator;
+    public double floatValue()
+    {
+        return (double)numerator / denominator;
     }
-} 
+    private int gcd(int a, int b)
+    {
+        if (b == 0) return a;
+        return gcd(b, a%b);
+    }
+    private void reduce()
+    {
+        int g = gcd(numerator, denominator);
+        numerator /= g;
+        denominator /= g;
+    }
+    public void multiply(Rational other)
+    {
+        numerator *= other.numerator;
+        denominator *= other.denominator;
+        reduce();
+    }
+    public void divide(Rational other)
+    {
+        numerator *= other.denominator;
+        denominator *= other.numerator;
+        reduce();
+    }
+}
